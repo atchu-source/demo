ae1207d40d61008eab2122c262141085a4dd9f80
atchaya1031@gmail.com <atchaya1031@gmail.com>
2026-08-19 22:29:33 +0530

    student rank calculation

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -5,58 +5,63 @@ import java.util.Scanner;
 public class Student {
 
     public void details() {
-      System.out.println("Student details");
+        System.out.println("Student details");
+        int student1 = 5;
+        Scanner sc = new Scanner(System.in);
 
-      Scanner sc = new Scanner(System.in);
-      System.out.println(" Enter your roll no:");
-      int rollno= sc.nextInt();
-      System.out.println(" Enter your name");
-      String name=sc.next();
+        System.out.println("Enter student count");
+        int count = sc.nextInt();
 
-      boolean pass = true;
+        for (int i = 5; i >= 0; i--) {
+            System.out.println(i);
 
-      System.out.println("mark 1");
-      int mark1=sc.nextInt();
+        System.out.println(" Enter your roll no:");
+        int rollno = sc.nextInt();
+        System.out.println(" Enter your name");
+        String name = sc.next();
 
-      if(mark1 < 35){
-          pass = false;
+        boolean pass = true;
+
+        System.out.println("mark 1");
+        int mark1 = sc.nextInt();
+
+        if (mark1 < 35) {
+            pass = false;
+        }
+
+        System.out.println("mark 2");
+        int mark2 = sc.nextInt();
+
+        if (mark2 < 35) {
+            pass = false;
+        }
+        System.out.println("mark 3");
+        int mark3 = sc.nextInt();
+        if (mark3 < 35) {
+            pass = false;
+        }
+        System.out.println("mark 4");
+        int mark4 = sc.nextInt();
+        if (mark4 < 35) {
+            pass = false;
+        }
+        System.out.println("mark 5");
+        int mark5 = sc.nextInt();
+
+        if (mark5 < 35) {
+            pass = false;
+        }
+        System.out.println("Total");
+        int total = mark1 + mark2 + mark3 + mark4 + mark5;
+        int avg = total / 5;
+
+        if (avg >= 80 && avg <= 100 && pass) {
+            System.out.println("First class");
+        } else if (avg >= 35 && avg <= 80 && pass) {
+            System.out.println("Second class");
+        } else if (avg >= 35 && !pass) {
+            System.out.println("Fail-Try to get more marks ");
+        }
       }
-
-      System.out.println("mark 2");
-      int mark2=sc.nextInt();
-
-        if(mark2 < 35){
-            pass = false;
-        }
-      System.out.println("mark 3");
-      int mark3=sc.nextInt();
-        if(mark3 < 35){
-            pass = false;
-        }
-      System.out.println("mark 4");
-      int mark4=sc.nextInt();
-        if(mark4 < 35){
-            pass = false;
-        }
-      System.out.println("mark 5");
-      int mark5=sc.nextInt();
-
-        if(mark5 < 35){
-            pass = false;
-        }
-      System.out.println("Total");
-      int total=mark1+mark2+mark3+mark4+mark5;
-      int avg=total/5;
-
-    if(avg >=80 && avg<=100 && pass){
-        System.out.println("First class");
-    }
-    else if (avg >= 35 && avg <=80 && pass) {
-        System.out.println("Second class");
-    }
-    else if (avg>=35 && !pass) {
-        System.out.println("Fail-Try to get more marks ");
-    }
-
     }
 }
